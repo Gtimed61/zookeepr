51d2c023261668b64a6d7362ffbe23a5e96f4bd4
Thorne <gthorne61@gmail.com>
2022-10-22 17:05:23 -0500

    params

--- server.js
+++ server.js
@@ -44,9 +44,23 @@ app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     res.json(animals);
 })
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
